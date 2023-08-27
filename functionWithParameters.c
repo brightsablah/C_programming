b9c64543d5298d081330389ef3f211715191f691
brightsablah <141350864+brightsablah@users.noreply.github.com>
2023-08-27 22:45:30 +0000

    Create functionWithParameters.c

---
+++ functionWithParameters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void greeting(name)
+{
+        printf("Hello to you %s\n", name);
+}
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  
+        greeting();
+
+return (0);
+
+}
