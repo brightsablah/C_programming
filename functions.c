b7aa630805d969169ad6fd434ddcf2b55a62c3a1
brightsablah <141350864+brightsablah@users.noreply.github.com>
2023-08-27 22:16:05 +0000

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void greeting()
+{
+        printf("Hello to you my friends\n");
+}
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        greeting();
+        greeting();
+        greeting();
+        greeting();
+        printf("all functions have been executed\n");
+
+return (0);
+
+}
