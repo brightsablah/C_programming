16702653cace7334bbd69a9668db3ec8b87a71fc
brightsablah <141350864+brightsablah@users.noreply.github.com>
2023-08-27 22:51:25 +0000

    Create numberSquare.c

---
+++ numberSquare.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int numberSquare(number)
+{
+  int result = number * number;
+  printf("Square of %d is %d", number, result);
+}
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  numbersquare(3)
+
+return (0);
+
+}
