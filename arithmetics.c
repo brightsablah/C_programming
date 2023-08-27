5b5f7138974957d69177dc68d46926c2b34995b4
Bright <bright.sablah@outlook.com>
2023-08-27 23:53:36 +0000

    defining arithmetic function

--- arithmetics.c
+++ arithmetics.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+int arithmetics(int num1, int num2)
+{
+	int sum = num1 + num2;
+	int dividend = num1 / num2;
+	int product = num1 * num2;
+	int difference = num1 - num2;
+	int modulus = num1 % num2;
+
+	printf("sum = %d\n", sum);
+	printf("dividend = %d\n", dividend);
+	printf("product = %d\n", product);
+	printf("difference = %d\n", difference;
+	printf("modulus = %d\n", modulus;
+}
+
 /**
 * main - Entry point
-* Description:
+* Description: function accepts two integers and returns simple arithmetic results
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-
+	arithmetics(12, 21);
 
 return (0);
 
