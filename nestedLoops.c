67ee80c4a9e20f08a51a6ce7cd2ece1627493c90
brightsablah <141350864+brightsablah@users.noreply.github.com>
2023-08-27 22:14:30 +0000

    Update nestedLoops.c

--- nestedLoops.c
+++ nestedLoops.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+/**
+* main - Entry point
+* Description: Simple demonstration of nested loops
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+        int i, j;
+        for (i = 0; i <= 4; i++)
+        {
+                printf("This is the STARTING of the outer loop %d\n", i);
+                        for (j = 0; j <= 4; j++)
+                        {
+                                printf("Inner Loop with number %d\n", j);
+                        }
+                printf("This is the Ending of the outer loop %d\n", i);
+        }
+
+return (0);
+
+}
