1a7e79ba0622986e9a89ca60d86100548c84c471
brightsablah <141350864+brightsablah@users.noreply.github.com>
2023-08-27 22:53:07 +0000

    Create inputAndOutput.c

---
+++ inputAndOutput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (sucess)
+*/
+int main(void)
+{
+        char f, m, l;
+        int age;
+
+        printf("Enter your initials,followed by your age: ");
+        scanf("%c %c %c %d", &f, &m, &l, &age);
+        printf("My initials are %c%c%c and my age is is %d.\n", f, m, l, age);
+        return (0);
+}
