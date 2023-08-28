2897c04dd80420491861c30637377f78eed0de79
Bright <bright.sablah@outlook.com>
2023-08-28 23:19:06 +0000

    Completed user input commands fgets and scanf

--- userinput.c
+++ userinput.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	char name[100];
+
+	printf("Fgets Prompt: Enter your name\n");
+	fgets(name, sizeof(name), stdin);
+	printf("Fget Input received is : %s\n", name);
+
 	printf("Scanf Prompt: Enter your name\n");
 	scanf("%s", name);
 	printf("Scanf Input received is : %s\n", name);
-
-	printf("Fgets Prompt: Enter your name\n");
-	fgets(name, sizeof(name), stdin;
-	printf("Fget Input received is : %s\n", name);
 
 return (0);
 
