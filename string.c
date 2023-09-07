e6d4be89ec8eafc6203477af44ef5353f31c8174
Bright <bright.sablah@outlook.com>
2023-09-07 00:01:45 +0000

    printing a string variable

---
+++ string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char name[50] = "Varuzovic";
+
+	printf("%s\n", name);
+
+return (0);
+
+}
