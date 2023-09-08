7a9170291de7b3b0a551359e7e7941d5b2aea0a7
Bright <bright.sablah@outlook.com>
2023-09-08 03:19:10 +0000

    array access

---
+++ array_access.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints contents of an array
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int age[5] = {31, 17, 12, 32, 27};
+
+	printf("%d ", age[0]);
+	printf("%d ", age[1]);
+	printf("%d ", age[2]);
+	printf("%d ", age[3]);
+	printf("%d ", age[4]);
+
+	printf("\n");
+return (0);
+
+}
