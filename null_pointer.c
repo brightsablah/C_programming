6bc7258ef17fb6a9953da54b818dfb6ff03c4d8e
Bright <bright.sablah@outlook.com>
2023-09-18 16:17:17 +0000

    added null pointers

---
+++ null_pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description:
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a = 3;
+	int *ptr1 = NULL;
+	int *ptr2;
+
+/* Print address stored in both pointers */
+	printf("%x\n", ptr1);
+	printf("%x\n", ptr2);
+
+return (0);
+
+}
