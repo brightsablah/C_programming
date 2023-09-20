2a7ad8212d28d83baf634007bd634d603d7c80ee
Bright <bright.sablah@outlook.com>
2023-09-20 22:10:40 +0000

    added times table made by for loops

---
+++ times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints times table of input number
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int num, soln, i;
+
+	printf("Enter a number: ");
+	scanf("%d", &num);
+
+	printf("Times Table for %d is below\n", num);
+	printf("---------------------------\n");
+
+	for (i = 0; i <= 12; i++)
+	{
+		soln = num * i;
+		printf("%d x %d = %d\n", num, i, soln);
+	}
+
+return (0);
+
+}
