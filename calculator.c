f4f8f1a9af67292416f258a9b15e56e29ea28fb7
Bright <bright.sablah@outlook.com>
2023-09-21 01:03:30 +0000

    added simple calculator made from switch statements update

--- calculator.c
+++ calculator.c
@@ -16,37 +16,41 @@ int main(void)
 	printf("Enter one operator ( + - * / )\n");
 	scanf("%c", &operator);
 
-	printf("Enter first operand (integer)");
+	printf("Enter first operand (integer): ");
 	scanf("%d", &a);
 
-	print("Enter second operand (integer)");
+	printf("Enter second operand (integer): ");
 	scanf("%d", &b);
 
 	switch (operator)
 	{
 		case '+':
 			{
-				printf("Sum = %d", a + b);
+				printf("Sum = %d\n", a + b);
+				break;
 			}
 
 		case '*':
 			{
-				printf("Product = %d", a * b);
+				printf("Product = %d\n", a * b);
+				break;
 			}
 
 		case '-':
 			{
-				printf("Difference = %d", a - b);
+				printf("Difference = %d\n", a - b);
+				break;
 			}
 
 		case '/':
 			{
-				printf("Quotient = %d", a / b);
+				printf("Quotient = %d\n", a / b);
+				break;
 			}
 
 		default:
 			{
-				printf("Enter valid operand ( + - * / ): \n");
+				printf("Invalid Operand Entered. Try again with valid operand ( + - * / ): \n");
 			}
 
 	}
