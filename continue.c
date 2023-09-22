da92237e2599da7758fb1916131bf723f6da1009
Bright <bright.sablah@outlook.com>
2023-09-22 00:52:24 +0000

    adding array initialization practice file

---
+++ continue.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: practicing to use continue statement in loops
+*					continue can only be used in loops and switch statements
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a, i, sum = 0;
+
+	for (i = 0; i <= 5; i++)
+	{
+		printf("Enter a number: \n");
+		scanf("%d", &a);
+
+		if (a < 0)
+		{
+			continue;
+		}
+
+		sum += a;
+	}
+
+	printf("Sum of positive integers you entered is: %d\n", sum);
+
+return (0);
+
+}
---
+++ readArray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: initializes array at runtime and prints values
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a[5], i;
+
+	printf("Enter 5 array elements: ");
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	printf("\nPrinting array Elements\n");
+	for (i = 0; i < 5; i++)
+	{
+		printf("array element at index %d is: %d\n", i, a[i]);
+	}
+
+	printf("\nPrinting array Elements in Reverse Order\n");
+	for (i = 4; i >= 0; i--)
+	{
+		printf("array element at index %d is: %d\n", i, a[i]);
+	}
+
+return (0);
+
+}
