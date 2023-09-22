4d6bb922465b8b723551a4f2933aed7131a8d239
Bright <bright.sablah@outlook.com>
2023-09-22 03:06:34 +0000

    added program that initializes a 2d array during runtime

---
+++ 2dArrayRuntimeInitialization.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: accepts input into a 2d array dring runtime
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int arr[2][3], i, j;
+
+	printf("Enter 6 numers: ");
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			scanf("%d", &arr[i][j]);
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+return (0);
+
+}
