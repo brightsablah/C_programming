5e30ba143c2596ce244bf8b7b8066fc00cce5d63
Bright <bright.sablah@outlook.com>
2023-09-22 01:10:03 +0000

    added program that checks for number of odd and even numbers in an array of elements

---
+++ oddEvenArray.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: initializes array at runtime and finds average of values
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a[5], i, odd = 0, even = 0;
+
+	printf("Enter 5 array elements: ");
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		if (a[i] % 2 == 0)
+		{
+			even++;
+		}
+		else
+		{
+			odd++;
+		}
+	}
+
+	printf("You entered %d even numbers and %d odd numbers\n", even, odd);
+
+return (0);
+
+}
