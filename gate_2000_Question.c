432e7b2d12da0d19cc23c0059253d64983a1a6cb
Bright <bright.sablah@outlook.com>
2023-09-24 16:26:25 +0000

    solved gate 2000 question

---
+++ gate_2000_Question.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int incr(int i)
+{
+	static int count = 0;
+
+	count = count + i;
+
+	return (count);
+}
+
+
+int main()
+{
+	int i, j;
+
+	for (i = 0; i <= 4; i++)
+	{
+		j = incr(i);
+	}
+
+	printf("%d\n", j);
+
+	return (0);
+}
