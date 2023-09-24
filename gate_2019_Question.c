5f1aa0667a0a8c1ffa2c59554a8042d39bd8bc05
Bright <bright.sablah@outlook.com>
2023-09-24 16:16:53 +0000

    solved gate 2019 question

---
+++ gate_2019_Question.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int jumble(int x, int y)
+{
+	x = 2 * x + y;
+	return (x);
+}
+
+
+int main()
+{
+	int x = 2, y = 5;
+
+	y = jumble(y,x);
+	x = jumble(y,x);
+
+	printf("%d\n", x);
+
+return (0);
+}
