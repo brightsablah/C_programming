d0e14bc5005f0c3408940d6c39ef0dad4d17ab90
Bright <bright.sablah@outlook.com>
2023-09-26 14:51:12 +0000

    non-tail recursion

---
+++ recursionNonTail.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int print(int a)
+{
+	if (a < 1) return 0;
+	else
+	{
+	return 1 + print(a / 2);
+	}
+}
+
+void main()
+{
+	int x;
+
+	x = print(10);
+	printf("%d\n", x);
+}
+
