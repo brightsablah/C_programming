3eef51f6ca4f4816e7d292b05a1eb154483692ed
Bright <bright.sablah@outlook.com>
2023-09-26 14:49:41 +0000

    recursion practice

---
+++ recursion1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum (int);
+
+int main()
+{
+	int a;
+	a = sum(5);
+	printf("%d\n", a);
+
+	return (0);
+}
+
+int sum(int x)
+{
+	int s = 0;
+	if(x == 1)
+	{
+		return (x);
+	}
+	s = x + sum(x -1);
+	return (s);
+}
