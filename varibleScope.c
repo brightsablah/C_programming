b6128add463a38964dea254312b69612b0a28fd7
Bright <bright.sablah@outlook.com>
2023-09-28 05:05:58 +0000

    variable scope concepts

--- varibleScope.c
+++ varibleScope.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-int i = 5;
-int k = 7;
+	int i = 5;
+	int k = 7int;
+
 int main()
 {
-int i = 90;
+	int i = 90;
 
-int *p = &k;
+	int *p = &k;
 	*p = 4;
 	printf("%d\n", i);
 	printf("%d\n", k);
+
 return 0;
 }
