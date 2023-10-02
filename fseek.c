9e9ce76435a5c06ac0b1b8e0810dc910e3afe50a
Bright <bright.sablah@outlook.com>
2023-10-02 07:18:36 +0000

    fseek

---
+++ fseek.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	FILE *fp = NULL;
+	char ch;
+
+	fp = fopen("textfile.txt","r+");
+	if (fp == NULL)
+	{
+		printf(" ERROR: File not Opened");
+		exit(1);
+	}
+
+	fseek(fp, 23, SEEK_SET);
+	ch = fgetc(fp);
+	printf("%c\n", ch);
+
+	fseek(fp, -13, SEEK_CUR);
+	ch = fgetc(fp);
+	printf("%c\n", ch);
+
+	fseek(fp, -25, SEEK_END);
+	ch = fgetc(fp);
+	printf("%c\n", ch);
+
+	fclose(fp);
+}
